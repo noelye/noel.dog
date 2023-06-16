1074b500dd12aa63a79bd94ca65e4086f570065f
noel <36994257+noelye@users.noreply.github.com>
2023-06-16 13:07:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
 import { STLLoader } from 'https://unpkg.com/three@0.127.0/examples/jsm/loaders/STLLoader.js';
-import { AsciiEffect } from 'https://unpkg.com/browse/three@0.127.0/examples/jsm/effects/AsciiEffect.mjs';
+import { AsciiEffect } from './AsciiEffect.js';
 
 // DECLARE / CREATE VARIABLES
 
