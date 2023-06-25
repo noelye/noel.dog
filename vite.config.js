4ecad380f7cb40946ae5c3d6dbb47ca8d427e837
Noel <genjified@gmail.com>
2023-06-25 15:57:24 -0400

    file restructure to allow for multiple pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
+
 export default defineConfig({
+  root,
   resolve: {
       extensions: ['.js','.mjs']
+    },
+  build: {
+    outDir, 
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about', 'index.html'),
+        send: resolve(root, 'send', 'index.html')
+      }
     }
+  }
 })
